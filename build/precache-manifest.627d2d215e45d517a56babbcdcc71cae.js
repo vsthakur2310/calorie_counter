229f16f2b3fc5b35496041861617bbbca0cc6c29
Vaibhav Singh <vsthakur2310@gmail.com>
2020-08-30 05:05:46 +0530

    Add files via upload

---
+++ build/precache-manifest.627d2d215e45d517a56babbcdcc71cae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b7ccf5117c3b1e6aee11f0c4facd3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9415c2c4ff95f6035e1",
+    "url": "/static/css/main.e1ddc785.chunk.css"
+  },
+  {
+    "revision": "1e5b4cd34da89aa2450e",
+    "url": "/static/js/2.1fbfe257.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1fbfe257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9415c2c4ff95f6035e1",
+    "url": "/static/js/main.9c3e0ef6.chunk.js"
+  },
+  {
+    "revision": "697ac4eed5564badcab6",
+    "url": "/static/js/runtime-main.cb89c3f2.js"
+  }
+]);
